04d3d9b722f703aa595d5cfd40e903addf9cb96e
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-02-25 17:38:24 +0900

    Tr요청, 예수금을 받아오는 부분에서 문제가 있음.

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -10,20 +10,24 @@ class Kiwoom(QAxWidget):
 
         ####### eventloop 모음
         self.login_event_loop = None
+        self.detail_account_info_event_loop = None
+        self.detail_account_info_event_loop_2 = None
+        # Event Loop 란 : 데이터를 처리하는 동안, 다른 작업을 할 수 있게 만드는 거.
+        # Pyqt Evnet Loop : 데이터를 처리하는 동안, 다른 것들이 실해 되지 않게, block을 건다.
         ####################
 
         ####### 변수 모음
         self.account_num = None
         ####################
 
+
         self.get_ocx_instance()
         self.event_slots()
 
         self.signal_login_commConnect()  # 여기까지 로그인
         self.get_account_info()          # 계좌 정보 실행
-
-        self.detail_account_info()       # 계좌정보 가져오기
-        #self.trdata_slot()               # 예수금 가져오기
+        self.detail_account_info()       # 예수금 가져오는거
+        self.detail_account_mystock()    # 계좌평가 잔고 내역 요청
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -64,6 +68,20 @@ class Kiwoom(QAxWidget):
         # ("내가 지은 요청이름", "TR번호", "preNext", "화면번호")
         self.dynamicCall("CommRqData(String, String, int, String)", "예수금상세현황요청", "opw00001", "0", "2000")
 
+        self.detail_account_info_event_loop = QEventLoop()
+        self.detail_account_info_event_loop.exec_()
+
+    def detail_account_mystock(self, sPreNext="0"):
+        print("계좌평가 잔고내역 요청")
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)  # 인적사항 기입
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "계좌평가잔고내역요청", "opw00018", sPreNext, "2000")
+
+        self.detail_account_info_event_loop_2 = QEventLoop()
+        self.detail_account_info_event_loop_2.exec_()
+
     def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
         '''
         Tr 요청을 받는 구역이다. 슬롯이다.
@@ -83,6 +101,21 @@ class Kiwoom(QAxWidget):
             print("출금가능금액 %s" % ok_deposit)
             print("출금가능금액 형변환 %s" % int(ok_deposit))
 
-# Event Loop 란 : 데이터를 처리하는 동안, 다른 작업을 할 수 있게 만드는 거.
-# Pyqt Evnet Loop : 데이터를 처리하는 동안, 다른 것들이 실해 되지 않게, block을 건다.
+            #이벤트 루프 끊기
+            self.detail_account_info_event_loop.exit()
+            # 위의 작업이 끝난뒤에 다른 작업들이 시작된다.
+
+        if sRQName == "계좌평가잔고내역요청":
+
+            total_buy_money = self.dynamicCall("GetCommDate(String, String, int, String)", sTrCode, sRQName, 0, "총매입금액")
+            total_buy_money_result = int(total_buy_money)
+
+            print("총매입금액 %s" % total_buy_money_result)
+
+            total_profit_loss_rate = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "총수익률(%)")
+            total_profit_rate_result = float(total_profit_loss_rate)
+
+            print("총수익률(%%) : %s" % total_profit_loss_rate_result)
+
+            self.detail_account_info_event_loop_2.exit()
 
