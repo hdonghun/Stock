539b055ade7fa81b7e5678227f29fd9e1b0faf3d
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-02-25 13:42:07 +0900

    로그인을 한 후, 나의 보유 계좌번호 가져오기 완성

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,5 +1,6 @@
 from PyQt5.QAxContainer import *
 from PyQt5.QtCore import * #로그인 중에 핸들링이 값이 없다고 나오는것을 해결해주기 위한
+from config.errorCode import * # errorCode파일을 가져오기
 class Kiwoom(QAxWidget):
     def __init__(self):
 
@@ -10,10 +11,16 @@ class Kiwoom(QAxWidget):
         ####### eventloop 모음
         self.login_event_loop = None
         ####################
+
+        ####### 변수 모음
+        self.account_num = None
+        ####################
+
         self.get_ocx_instance()
         self.event_slots()
 
-        self.signal_login_commConnect()
+        self.signal_login_commConnect()  # 여기까지 로그인
+        self.get_account_info()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -29,10 +36,17 @@ class Kiwoom(QAxWidget):
                                         # 자동 로그인 방법은 코드로 하지 않고, KOAStudio에서 설정하는 방법으로
     def login_slot(self, errCode):
         print(errCode)
+        print(errors(errCode))         #errorCode
 
         self.login_event_loop.exit()
 
 
+    #내 정보 가져오기
+    def get_account_info(self):
+        account_list = self.dynamicCall("GetLoginInfo(String)","ACCNO")  # 계좌번호
+
+        self.account_num = account_list.split(';')[0] #계좌번호를 self에 넣어 언제든지 사용할수 있게 만들어준다.
+        print("나의 보유 계좌번호 %s " % self.account_num) #계좌번호 확인
 
 
 
